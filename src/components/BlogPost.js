9d10606dccc64342854d3606c9cf1c0ef24d0d98
auma2022 <auma.amwayi@student.moringaschool.com>
2022-07-20 12:43:53 +0300

    add comments

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -2,6 +2,15 @@ import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
+/*
+ Importing React and ReactDOM
+2. Rendering a BlogPost component with a default article text of "Dear Reader: Bjarne Stroustrup has the perfect lecture oration."
+3. Defining a Comment component and rendering it inside of BlogPost.
+4. Importing stylesheet code from the blog_app.less file (which lives in the same directory as blog_app.py).
+5. Adding a comment element to the blog post with a comment text of "I agree with this statement. - Angela Merkel".
+6. Adding another comment element to the blog post with a comment text of "A universal truth. - Noam Chomsky".
+7. Adding another comment element to the blog post with a comment text of "Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451". */
+
 function BlogPost() {
   return (
     <div id="blog-post">
